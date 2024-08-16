98e202c658d01e458c512c17eca7156255eb9112
rgayvert <rgayvert@pm.me>
2024-08-16 19:06:11 -0400

    added support for hash routing

--- assets/source/Demos/Text/MarkdownExample.ts
+++ assets/source/Demos/Text/MarkdownExample.ts
@@ -1,10 +1,10 @@
-import { View, core, em, ScrollPanel, pct, atom } from "zaffre";
+import { View, core, em, ScrollPanel, pct } from "zaffre";
 import { Markdown } from "../Common";
 
 export function MarkdownExample(): View {
   return ScrollPanel({ width: pct(100), height: pct(100) }).append(
     Markdown({
-      uri: "info/markdownit-examples.md",
+      uri: "url.markdownit-examples", 
       border: core.border.thin,
       padding: core.space.s4,
       height: em(20),
